088627e6de599ad510e54f18c323ae70bad576a7
Justin Crawford <Justin@stacksmash.net>
2020-09-21 12:55:52 -0700

    sm8150-common: disable proton clang for now
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,9 +52,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
 TARGET_KERNEL_CONFIG := weeb_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+# TARGET_KERNEL_CLANG_COMPILE := true
+# TARGET_KERNEL_CLANG_VERSION := proton
+# KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Platform
